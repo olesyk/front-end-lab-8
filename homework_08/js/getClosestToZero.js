d4934ebe770b1dffea81219b4581c7ce2f6922bc
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-15 12:42:50 +0200

    changed code
    no need to return two closest numbers if they just  have different  sign, enough to return first of them, founded among values.

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,11 +1,6 @@
+'use strict'
 const getClosestToZero = (...values) => {
-	let negativeValues = values.filter(n => n<0).sort((a,b) => b-a);
-	let positiveValues = values.filter(n => n>=0).sort((a,b) => a-b);
-	if (Math.abs(negativeValues[0]) > Math.abs(positiveValues[0])) {
-		return positiveValues[0];
-	} else if (Math.abs(negativeValues[0]) < Math.abs(positiveValues[0])) {
-		return negativeValues[0];
-	} else {
-		return `${negativeValues[0]}, ${positiveValues[0]}`;
-	}
+	const uniq = Array.from(new Set(values));
+	//'Set' because there were incorrect results during sorting a lot of ununiq values;
+	return uniq.sort((a, b) => Math.abs(a) - Math.abs(b))[0];
 }
