8984bb780b02ef4030c3fb84f4f0b74bdf7f331d
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-18 20:20:06 +0200

    shorted code

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,6 +1,2 @@
 'use strict'
-const getClosestToZero = (...values) => {
-	const uniq = Array.from(new Set(values));
-	//'Set' because there were incorrect results during sorting a lot of ununiq values;
-	return uniq.sort((a, b) => Math.abs(a) - Math.abs(b))[0];
-}
+const getClosestToZero = (...values) => values.sort((a,b) => Math.abs(a) - Math.abs(b))[0];
--- homework_08/js/isSmaller.js
+++ homework_08/js/isSmaller.js
@@ -1,3 +1,3 @@
 'use strict'
 const isBigger = (a, b) => a > b;
-const isSmaller = (a, b) => a !== b ? !isBigger(a, b) : false;
+const isSmaller = (a, b) => a !== b && !isBigger(a, b);
