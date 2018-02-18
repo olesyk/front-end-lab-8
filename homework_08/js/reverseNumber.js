c68466ebb9d3483efb61f791a442f0ef06b69048
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-18 20:20:37 +0200

    shorted code

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,12 +1,8 @@
 'use strict'
 const reverseNumber = num => {
 	const arrayOfNumbers = num.toString().split("");
-	if (arrayOfNumbers[0] === "-") {
-		const reverseArray = arrayOfNumbers.slice(1).reverse();
-		reverseArray.unshift(arrayOfNumbers[0]);
-		num = Number(reverseArray.join(''));
-	} else {
-		num = Number(arrayOfNumbers.reverse().join(''));
-	}
-	return num;
+	const firstChar = arrayOfNumbers.shift();
+	const insertFirstCharAt = firstChar === '-' || firstChar === '+' ? 0 : arrayOfNumbers.length;
+	arrayOfNumbers.reverse().splice(insertFirstCharAt, 0, firstChar);
+	return Number(arrayOfNumbers.join(''));
 }
