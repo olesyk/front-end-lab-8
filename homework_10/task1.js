602d53f2b9415d08aea11d407231f7eea74bd473
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-21 21:15:33 +0200

    added task2

--- homework_10/task1.js
+++ homework_10/task1.js
@@ -1,8 +1,41 @@
 function debounce(callback, wait) {
   let timeout; 
-  const later = () => callback.apply(iterator = 1);
-    return function() {
+  return function() {
     clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
+    timeout = setTimeout(callback, wait);
   }
 }
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
