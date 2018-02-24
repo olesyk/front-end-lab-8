10f76a21ae02f225254ba5d7bb7af8106b8f0fee
Olesia Davniuk <lv250385sov@gmail.com>
2018-02-24 09:20:53 +0200

    formatted code

--- homework_11/app.js
+++ homework_11/app.js
@@ -5,25 +5,24 @@ function createFileTree(arr) {
 	var ul = document.createElement('ul');
 	for (var i = 0; i < arr.length; i++) {
 		var li = document.createElement('li');
-		li.innerHTML = arr[i].folder ? 
-		'<div class="grey push"><i class="material-icons folder-icon">folder</i>' + arr[i].title + '</div>': 
-		'<div class="grey"><i class="material-icons file-icon">insert_drive_file</i>' + arr[i].title + '</div>';
+		li.innerHTML = arr[i].folder ?
+			'<div class="grey push"><i class="material-icons folder-icon">folder</i>' + arr[i].title + '</div>' :
+			'<div class="grey"><i class="material-icons file-icon">insert_drive_file</i>' + arr[i].title + '</div>';
 		if (arr[i].children) {
 			var childrenUl = createFileTree(arr[i].children);
 			if (childrenUl) {
 				var div = document.createElement('div');
-				div.className = 'fg';
 				div.style.display = 'none';
 				div.appendChild(childrenUl);
 				li.appendChild(div);
 			}
 		}
 		if (!arr[i].children && arr[i].folder) {
-			var warn = document.createElement('div');
-			warn.className ='warnning';
-			warn.style.display = 'none';
-			warn.innerHTML = 'Folder is empty';
-			li.appendChild(warn);
+			var div = document.createElement('div');
+			div.className = 'warnning';
+			div.style.display = 'none';
+			div.innerHTML = 'Folder is empty';
+			li.appendChild(div);
 		}
 		ul.appendChild(li);
 	}
